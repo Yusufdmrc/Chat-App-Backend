3b19c59c5daa3aba548d5d36d78e17718af3bf63
yusuf <yusuffdemirci10@gmail.com>
2024-06-23 16:44:24 +0300

    Entities created

---
+++ ChatApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp;
+[Route("api/[controller]")]
+[ApiController]
+public class HomeControllers : Controller
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello World";
+    }
+
+}
